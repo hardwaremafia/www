9383083ec30956d13ba24a9ed6b44163e897267a
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2023-12-16 12:31:06 +0530

    adding site related content (incomplete)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 export default defineConfig({
-  site: 'https://lexingtonthemes.com',
+  site: 'https://thehardwaremafia.com',
   integrations: [tailwind(), sitemap(), ]
 });
---
+++ src/site.ts
@@ -0,0 +1,9 @@
+export const site = {
+    name: "Hardware Mafia",
+    cta: "Join The Mafia",
+    homeUrl: "https://thehardwaremafia.com",
+    meetupUrl: "https://lu.ma/hwmafia",
+    whatsAppUrl: "https://chat.whatsapp.com/HdCgjEg06cQ3ovVjGtFLwE",
+    twitter: "https://twitter.com/HardwareMafia",
+    github: "https://github.com/HardwareMafia",
+};
