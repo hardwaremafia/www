9c53fcd57b99441367c5f30e066aa9b4b3e619ee
Harsh Bhikadia <harsh.bhikadiya@gmail.com>
2024-04-23 01:46:50 +0530

    fix: update meetup url

--- src/site.ts
+++ src/site.ts
@@ -2,7 +2,7 @@ export const site = {
     name: "Hardware Mafia",
     cta: "Join The Mafia",
     homeUrl: "https://thehardwaremafia.com",
-    meetupUrl: "https://lu.ma/hwmafia",
+    eventsUrl: "https://lu.ma/hardwaremafia",
     whatsAppUrl: "https://chat.whatsapp.com/HdCgjEg06cQ3ovVjGtFLwE",
     twitter: "https://twitter.com/HardwareMafia",
     github: "https://github.com/HardwareMafia",
