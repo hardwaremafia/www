8582377c0bc73e3dfc7331c7da19f9521c824da6
Harsh Bhikadia <harsh@bhikadia.com>
2026-07-07 16:47:19 +0530

    feat: tailwind upgrade to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  site: 'https://thehardwaremafia.com',
-  integrations: [tailwind(), sitemap(), ]
+  site: "https://thehardwaremafia.com",
+  integrations: [sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
